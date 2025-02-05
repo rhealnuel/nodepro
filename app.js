fd4d3e2b325457721290abcf0f2f3cd6d6f043fb
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2025-02-05 18:19:22 +0100

    origi

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors());
+app.use(cors({ origin: "*" })); // Allow all origins
 
 // If you want to allow credentials (cookies, authorization headers, etc.)
 // app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: "Content-Type,Authorization" }));
