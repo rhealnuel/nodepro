ed814dacb394b544fe00940c834b8dee88b8c883
emmanuelkawekwune <emmanuelkawekwune@punch.agency>
2025-02-05 18:02:29 +0100

    hhh

--- app.js
+++ app.js
@@ -13,16 +13,10 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-// app.use(cors());
+app.use(cors());
 
-// // If you want to allow credentials (cookies, authorization headers, etc.)
+// If you want to allow credentials (cookies, authorization headers, etc.)
 // app.use(cors({ origin: "*", methods: "GET,POST,PUT,DELETE", allowedHeaders: "Content-Type,Authorization" }));
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-  });
 app.use('/uploads', express.static('uploads'))
 
 //routes
